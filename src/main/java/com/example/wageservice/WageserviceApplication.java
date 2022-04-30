03dd6333892a6c09f478b362a30f6b2399c54cab
Karel D'haene <karel.dhaene@gmail.com>
2022-04-30 14:52:12 +0200

    Initial blank spring boot project

---
+++ src/main/java/com/example/wageservice/WageserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.wageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WageserviceApplication.class, args);
+	}
+
+}
